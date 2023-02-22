b75af01c66484d3ec1d5e4473a34e39f578086c0
Read Langworthy <rlangworthy@gmail.com>
2023-02-21 21:02:27 -0800

    day 5 assignment done

--- day_5_assignment.js
+++ day_5_assignment.js
@@ -220,3 +220,56 @@ function testMostFreq(array) {
 testMostFreq([1,1,2,2,2,2,3,4,5,5])
 testMostFreq([1,1,2,2,2,2,3,4,5,5,5,5,5,5,5])
 
+
+
+// PROBLEM 3 - BONUS PROBLEM
+// I can solve it with brute force but can't figure it out in linear time!
+// I would love a hint.
+
+function maxAreaBrute(array) {
+    // brute force version - runs in O(n^2) time bc of nested for loops
+    if (array.length < 2) return undefined;
+
+    let maxArea = 0
+
+    for (let i = 0; i < array.length-1; i++) {
+        for (let j = i+1; j < array.length; j++) {
+            let tempArea = Math.min(array[i], array[j]) * (j-i)
+            if (tempArea > maxArea) maxArea = tempArea
+        }
+    }
+    return maxArea
+}
+
+console.log(maxAreaBrute([1,8,6,2,5,4,8,3,7])) // 49
+console.log(maxAreaBrute([1,8,6,2,5,4,8,3,5])) // 40
+console.log(maxAreaBrute([1,8,6,2,5,4,8,3,8])) // 56
+
+
+function maxAreaTwo(arr) {
+    // trying to run in O(n) time
+    // I can't quite get this to work.
+    // I thought some sort of multiple pointers or sliding window would work, but can't get it...
+    // I would love a hint.
+
+    if (arr.length < 2) return undefined;
+
+    let maxArea = 0
+    let l = 0, r = arr.length-1
+
+    while (l < r) {
+        let tempArea = Math.min(arr[l], arr[r]) * (r - l)
+        if (tempArea > maxArea) maxArea = tempArea
+
+        // this logic does NOT solve it
+        if (arr[l] < arr[l+1]) l++ 
+        else r--
+    }
+
+    return maxArea
+}
+
+console.log(maxAreaTwo([1,8,6,2,5,4,8,3,7])) // 49
+console.log(maxAreaTwo([1,8,6,2,5,4,8,3,5])) // 40
+console.log(maxAreaTwo([1,8,6,2,5,4,8,3,8])) // 56
+console.log(maxAreaTwo([1,8,900,1000,5,4,8,3,8])) // 900 - fails on this example
