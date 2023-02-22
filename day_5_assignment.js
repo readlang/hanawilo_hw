c2de92810e2ee933c7eba4a1dbe3e9c47a297ecb
Read Langworthy <rlangworthy@gmail.com>
2023-02-22 10:49:04 -0800

    day 5 corrected

--- day_5_assignment.js
+++ day_5_assignment.js
@@ -244,28 +244,23 @@ function maxAreaBrute(array) {
 console.log(maxAreaBrute([1,8,6,2,5,4,8,3,7])) // 49
 console.log(maxAreaBrute([1,8,6,2,5,4,8,3,5])) // 40
 console.log(maxAreaBrute([1,8,6,2,5,4,8,3,8])) // 56
+console.log("-------")
 
 
 function maxAreaTwo(arr) {
     // trying to run in O(n) time
-    // I can't quite get this to work.
-    // I thought some sort of multiple pointers or sliding window would work, but can't get it...
-    // I would love a hint.
-
     if (arr.length < 2) return undefined;
 
-    let maxArea = 0
-    let l = 0, r = arr.length-1
+    let maxArea = 0,  l = 0, r = arr.length-1
 
     while (l < r) {
         let tempArea = Math.min(arr[l], arr[r]) * (r - l)
-        if (tempArea > maxArea) maxArea = tempArea
+        maxArea = Math.max( tempArea, maxArea )
 
-        // this logic does NOT solve it
-        if (arr[l] < arr[l+1]) l++ 
+        // if value of L is less, L++
+        if (arr[l] <= arr[r]) l++ 
         else r--
     }
-
     return maxArea
 }
 
